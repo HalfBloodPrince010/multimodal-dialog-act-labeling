524f755c5d9e404a8ac34f08bcc458bfe4407f10
Prashanth Pujar <ppujar@usc.edu>
2021-02-10 14:07:49 -0800

    Adding configuration details

---
+++ config.py
@@ -0,0 +1,33 @@
+import torch
+import os
+
+config = {
+    
+    # Data 
+    "data_dir":os.path.join(os.getcwd(), 'data'),
+    "dataset":"switchboard",
+    "text_field":"",
+    "label_field":"",
+
+    "max_len":256,
+    "batch_size":64,
+    "num_workers":4,
+    
+    # Model
+    "model_name":"",
+    "hidden_size":768,
+    "num_classes":43, # there are 43 classes in switchboard corpus
+    
+    # training
+    "save_dir":"./",
+    "project":"dialogue-act-label-classification",
+    "run_name":"context-aware-attention-dac",
+    "lr":1e-5,
+    "monitor":"val_accuracy",
+    "min_delta":0.001,
+    "filepath":"./checkpoints/{epoch}-{val_accuracy:4f}",
+    "precision":32,
+    "average":"micro",
+    "epochs":100,
+    "device":torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+}
